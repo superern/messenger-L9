eafb39abcbb88da9f45bd6b3d18b50cc3ad4a5e9
Ern Gregorio <ernandrewgregorio@gmail.com>
2022-03-21 05:52:32 +0800

    fix models

---
+++ app/Events/NewMessage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use JetBrains\PhpStorm\ArrayShape;
+
+class NewMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public Message $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Message $message
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|PrivateChannel|array
+     */
+    public function broadcastOn(): Channel|PrivateChannel|array
+    {
+        return new PrivateChannel('messages.'. $this->message->to);
+    }
+
+    #[ArrayShape(['message' => "\App\Models\Message"])]
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => $this->message
+        ];
+    }
+}
