28134e268f2b6b22166f4e031dbcbded3d8b40c5
Ern Gregorio <ernandrewgregorio@gmail.com>
2022-03-21 05:52:48 +0800

    use messages->to identifier in broadcast

--- routes/channels.php
+++ routes/channels.php
@@ -10,9 +10,9 @@ use Illuminate\Support\Facades\Broadcast;
 | Here you may register all of the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
-|
+| {id} = message sent to user id
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+Broadcast::channel('messages.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
